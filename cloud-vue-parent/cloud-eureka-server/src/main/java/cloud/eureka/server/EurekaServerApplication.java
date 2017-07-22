63440956b710bcc34d1ed701254e8fcc2f2a624e
liangchengcheng <1038127753@qq.com>
2017-07-22 16:06:36 +0800

    add config file
    add config file

---
+++ cloud-vue-parent/cloud-eureka-server/src/main/java/cloud/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package cloud.eureka.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by liangchengcheng on 2017/7/22.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
+    }
+}
