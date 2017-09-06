19896dd53d460cd264b639c35243caaa8a4f042b
liangchengcheng <1038127753@qq.com>
2017-09-07 06:25:11 +0800

    add js file
    add js file

---
+++ cloud-vue/my-vue-cloud/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+<style lang="scss">
+
+</style>
+<script>
+    export default {
+        props: {},
+        data() {
+            return {}
+        },
+        watch: {}
+    }
+</script>
