e994fe902f3c2e1985b10925c949112e046b537f
liangchengcheng <1038127753@qq.com>
2017-09-06 22:07:47 +0800

    main.js
    main.js

--- cloud-vue/my-vue-cloud/src/components/Account/Login.vue
+++ cloud-vue/my-vue-cloud/src/components/Account/Login.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <el-form :model="form" :rules="rules2" ref="form" label-position="left" label-width="0px" class="demo-ruleForm card-box loginform">
+    <el-form :model="form" :rules="rules2" ref="form" label-position="left" label-width="0px"
+             class="demo-ruleForm card-box loginform">
       <h3 class="title">{{systemName}}</h3>
       <el-form-item prop="username">
         <el-input type="text" v-model="form.username" auto-complete="off" placeholder="账号"></el-input>
@@ -14,7 +15,9 @@
       </el-form-item>
       <el-checkbox v-model="checked" style="margin:0px 0px 35px 0px;">记住密码</el-checkbox>
       <el-form-item style="width:100%;">
-        <el-button type="primary" style="width:100%;" v-loading="loading" @click.native.prevent="handleSubmit2('form')">登录</el-button>
+        <el-button type="primary" style="width:100%;" v-loading="loading" @click.native.prevent="handleSubmit2('form')">
+          登录
+        </el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -25,111 +28,111 @@
 
   export default {
     data() {
-    return {
-      title: '',
-      systemName: '',
-      loading: false,
-      form: {
-        username: '',
-        password: '',
-        verifyCode: ''
-      },
-      requireVerify: false,
-      verifyUrl: '',
-      verifyImg: window.HOST + 'admin/verify',
-      rules2: {
-        username: [
-          { required: true, message: '请输入账号', trigger: 'blur' }
-        ],
-        password: [
-          { required: true, message: '请输入密码', trigger: 'blur' }
-        ],
-        verifyCode: [
-          { required: false, message: '请输入验证码', trigger: 'blur' }
-        ]
-      },
-      checked: false
-    }
-  },
-  methods: {
-    refreshVerify() {
-      this.verifyUrl = ''
-      setTimeout(() => {
-        this.verifyUrl = this.verifyImg + '?v=' + moment().unix()
-    }, 300)
-  },
-  handleSubmit2(form) {
-    if (this.loading) return
-    this.$refs.form.validate((valid) => {
-      if (valid) {
-      this.loading = !this.loading
-      let data = {}
-      if (this.requireVerify) {
-        data.username = this.form.username
-        data.password = this.form.password
-        data.verifyCode = this.form.verifyCode
-      } else {
-        data.username = this.form.username
-        data.password = this.form.password
+      return {
+        title: '',
+        systemName: '',
+        loading: false,
+        form: {
+          username: '',
+          password: '',
+          verifyCode: ''
+        },
+        requireVerify: false,
+        verifyUrl: '',
+        verifyImg: window.HOST + 'admin/verify',
+        rules2: {
+          username: [
+            {required: true, message: '请输入账号', trigger: 'blur'}
+          ],
+          password: [
+            {required: true, message: '请输入密码', trigger: 'blur'}
+          ],
+          verifyCode: [
+            {required: false, message: '请输入验证码', trigger: 'blur'}
+          ]
+        },
+        checked: false
       }
-      if (this.checked) {
-        data.isRemember = 1
-      } else {
-        data.isRemember = 0
-      }
-      this.apiPost('admin/login', data).then((res) => {
-        if (res.code != 200) {
-        this.loading = !this.loading
-        this.handleError(res)
-      } else {
-        this.refreshVerify()
-        if (this.checked) {
-          Cookies.set('rememberPwd', true, { expires: 1 })
+    },
+    methods: {
+      refreshVerify() {
+        this.verifyUrl = ''
+        setTimeout(() => {
+          this.verifyUrl = this.verifyImg + '?v=' + moment().unix()
+        }, 300)
+      },
+      handleSubmit2(form) {
+        if (this.loading) return
+        this.$refs.form.validate((valid) => {
+          if (valid) {
+            this.loading = !this.loading
+            let data = {}
+            if (this.requireVerify) {
+              data.username = this.form.username
+              data.password = this.form.password
+              data.verifyCode = this.form.verifyCode
+            } else {
+              data.username = this.form.username
+              data.password = this.form.password
+            }
+            if (this.checked) {
+              data.isRemember = 1
+            } else {
+              data.isRemember = 0
+            }
+            this.apiPost('admin/login', data).then((res) => {
+              if (res.code != 200) {
+                this.loading = !this.loading
+                this.handleError(res)
+              } else {
+                this.refreshVerify()
+                if (this.checked) {
+                  Cookies.set('rememberPwd', true, {expires: 1})
+                }
+                this.resetCommonData(res.data)
+                _g.toastMsg('success', '登录成功')
+              }
+            })
+          } else {
+            return false
+          }
+        })
+      },
+      checkIsRememberPwd() {
+        if (Cookies.get('rememberPwd')) {
+          let data = {
+            rememberKey: Lockr.get('rememberKey')
+          }
+          this.apiPost('admin/relogin', data).then((res) => {
+            this.handelResponse(res, (data) => {
+              this.resetCommonData(data)
+            })
+          })
         }
-        this.resetCommonData(res.data)
-        _g.toastMsg('success', '登录成功')
       }
-    })
-  } else {
-    return false
-  }
-  })
-  },
-  checkIsRememberPwd() {
-    if (Cookies.get('rememberPwd')) {
-      let data = {
-        rememberKey: Lockr.get('rememberKey')
-      }
-      this.apiPost('admin/relogin', data).then((res) => {
+    },
+    created() {
+      this.checkIsRememberPwd()
+      this.apiPost('admin/configs').then((res) => {
         this.handelResponse(res, (data) => {
-        this.resetCommonData(data)
-    })
-  })
-  }
-  }
-  },
-  created() {
-    this.checkIsRememberPwd()
-    this.apiPost('admin/configs').then((res) => {
-      this.handelResponse(res, (data) => {
-      document.title = data.SYSTEM_NAME
-    this.systemName = data.SYSTEM_NAME
-    if (parseInt(data.IDENTIFYING_CODE)) {
-      this.requireVerify = true
-      this.rules2.verifyCode[0].required = true
-    }
-  })
-  })
-  this.verifyUrl = this.verifyImg
-  },
-  mounted() {
-    window.addEventListener('keyup', (e) => {
-      if (e.keyCode === 13) {
-      this.handleSubmit2('form')
-    }
-  })
-  },
-  mixins: [http]
+          document.title = data.SYSTEM_NAME
+          this.systemName = data.SYSTEM_NAME
+          if (parseInt(data.IDENTIFYING_CODE)) {
+            this.requireVerify = true
+            this.rules2.verifyCode[0].required = true
+          }
+        })
+      })
+      this.verifyUrl = this.verifyImg
+    },
+    mounted() {
+      window.addEventListener('keyup', (e) => {
+        if (e.keyCode === 13) {
+          this.handleSubmit2('form')
+        }
+      })
+    },
+    mixins: [http]
   }
 </script>
 
@@ -139,6 +142,7 @@
     right: 100px;
     top: 0px;
   }
+
   .card-box {
     padding: 20px;
     /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
